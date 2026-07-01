c327a0b90b04494ea8ab4c8f99bad81cc18be661
Girish.G.R <girishgr2014@gmail.com>
2026-07-01 20:34:56 +0530

    Update activity2.py

--- activity2.py
+++ activity2.py
@@ -1,1 +1,9 @@
+Amount =int(input("Please Enter your Amount for Withdraw :"))
 
+note1 = Amount//100
+note2 = (Amount%100)//50
+note3 = ((Amount&100)%50)//10
+
+print("notes of 100 rupees", note1)
+print("notes of 50 rupees", note2)
+print("notes of 10 rupees", note3)
