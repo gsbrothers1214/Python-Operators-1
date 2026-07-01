b84fa3033c4715c46a440183eefe418c2cd6e78d
Girish.G.R <girishgr2014@gmail.com>
2026-07-01 20:55:13 +0530

    Update activity3.py

--- activity3.py
+++ activity3.py
@@ -1,1 +1,15 @@
+print("Enter Marks Obtained in 4 Subjects")
+math =int(input("maths :"))
+english =int(input("english :"))
+physics =int(input("physics :"))
+chemistry =int(input("chemistry :"))
+socialscience =int(input("social science :"))
+tamil =int(input("tamil :"))
 
+sum = math+english+physics+chemistry+socialscience+tamil
+print("Math , English , Physics , Chemistry , Social Science , Tamil")
+
+perc = (sum/600)*100
+
+print("Percentage Mark = ")
+print(perc)
