249c1e620c32b99940053a3c48e00aee628dd510
Girish.G.R <girishgr2014@gmail.com>
2026-07-01 20:27:35 +0530

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -1,1 +1,15 @@
+# Storing Values
+tree1 = 67
+tree2 = 98
+tree3 = 100
+tree4 = 500
+tree5 = 99
+tree6 = 44
 
+# Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5+tree6
+print("The Sum of all the 6 Trees is: ", sum)
+
+# Finding the Average of Trees
+average = sum/5
+print("The Average of the all Trees is: ", average)
